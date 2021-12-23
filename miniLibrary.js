10370ec5b9c7f388de26bc366a879c6400e2d0df
fahmi3232 <fahimun32@gmail.com>
2021-12-23 15:50:54 +0700

    Tugas Basic OOP - Mini Library

---
+++ miniLibrary.js
@@ -0,0 +1,12 @@
+new gridjs.Grid({
+    columns: ["Name", "Email", "Phone Number","Alamat"],
+    pagination:true,
+    sort:true,
+    data: [
+      ["Fahmi", "fahimun32@gmail.com", "085268477587", "Madiun"],
+      ["Gama", "gama123@gmail.com", "089519813988", "Bandung"],
+      ["Indra", "indra222@gmail.com", "085277648798", "Jakarta"],
+      ["Miftarif", "miftarif@gmail.com", "087784493444", "Pekalongan"],
+      ["Rhesa", "rhesa111@gmail.com", "08985554431", "Banyumas"],
+    ]
+}).render(document.getElementById("wrapper"));
